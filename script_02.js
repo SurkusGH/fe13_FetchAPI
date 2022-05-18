d123adfecbad305ca911d2ceb589aa64650c401e
SurkusGH <v.surkus@me.com>
2022-05-18 19:57:31 +0300

    Dont want to remember...

--- script_02.js
+++ script_02.js
@@ -1,5 +1,35 @@
 function FetchJokes(){
-    fetch('http://api.icndb.com/jokes/random/')
+  const divForContent = document.createElement('div')
+  function getRandomNum(max){
+    return Math.floor(Math.random() * max);
+  }
+
+  fetch('https://picsum.photos/v2/list?page=31')
+  .then((response) => {
+    if (response.ok) {
+      console.log('oky');
+      return response.json();
+    } else {
+      console.log('not okay');
+    }
+  })
+  .then((data) => {
+    
+    let pic = document.createElement ('img');
+    pic.src = data[getRandomNum(10)].download_url
+    pic.style.width = '50px';
+    pic.style.height = '50px';
+
+    console.log(data)
+    divForContent.appendChild(pic)
+    //document.body.append(pic)
+
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+
+    fetch('https://api.chucknorris.io/jokes/random')
     .then((response) => {
       if (response.ok) {
         console.log('ok');
@@ -11,12 +41,14 @@ function FetchJokes(){
     .then((data) => {
 
       const placeForJokes = document.querySelector('#placeForJokes')
-      const divForContent = document.createElement('div')
+      
       const fetchedJokeP = document.createElement ('p');
       const fetchedLink = document.createElement('a')
 
-      fetchedLink.textContent = data.json
-      fetchedJokeP.textContent = data.value.joke
+      fetchedLink.textContent = '->LINK<-'
+      fetchedLink.href=data.url
+      fetchedJokeP.textContent = data.value
+      divForContent.appendChild(fetchedLink)
       divForContent.appendChild(fetchedJokeP)
       placeForJokes.appendChild(divForContent)
 
@@ -29,14 +61,13 @@ function FetchJokes(){
   }
 
 
-  
-
   const add = document.querySelector('#add')
   let i = 1;
   add.addEventListener("click", () => {
-  FetchJokes();
-  document.getElementById("result").innerHTML = `Number of Jokes: ${i++}`
+    FetchJokes();
+    document.getElementById("result").innerHTML = `Number of Jokes: ${i++}`
   })
+
   // fetch('https://picsum.photos/v2/list?page=31')
   //   .then((response) => {
   //     if (response.ok) {
@@ -57,7 +88,7 @@ function FetchJokes(){
   //   data.forEach((imageObject) => {
   //     const image = document.createElement ('img');
   //     image.src = imageObject.download_url;
-  //     image.style.width = '200px';
+  //     image.style.width = '50px';
   //     document.body.append(image);
   //   })
   // }
